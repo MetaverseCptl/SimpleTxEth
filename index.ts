6903584b7e5ee54a9add8e3e965f0c9cc53435dd
MetaverseCptl <130099868+MetaverseCptl@users.noreply.github.com>
2024-04-04 16:30:19 +0200

    optimize

--- index.ts
+++ index.ts
@@ -7,18 +7,18 @@ const toAddress = '0x5678...';
 const privateKey = 'abcd....';
 
 async function sendEth() {
-  const transactionCount = await web3.eth.getTransactionCount(myAddress);
-    
-  const txData = {
-    nonce:    web3.utils.toHex(transactionCount),
-    gasLimit: web3.utils.toHex(25000),
-    gasPrice: web3.utils.toHex(10e9),
-    to:       toAddress,
-    from:     myAddress,
-    value:    web3.utils.toHex(web3.utils.toWei('0.01', 'ether'))
-  };
-
   try {
+    const transactionCount = await web3.eth.getTransactionCount(myAddress);
+
+    const txData = {
+      nonce:    web3.utils.toHex(transactionCount),
+      gasLimit: web3.utils.toHex(25000),
+      gasPrice: web3.utils.toHex(10e9),
+      to:       toAddress,
+      from:     myAddress,
+      value:    web3.utils.toHex(web3.utils.toWei('0.01', 'ether'))
+    };
+      
     const signedTransaction = await web3.eth.accounts.signTransaction(txData, privateKey);
       
     const txReceipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction as string);
